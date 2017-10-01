d0fddef6a427845eba0cdae4e2a79d012a0aaafd
Oleksii Kozhyn <suburbakskin@gmail.com>
2017-10-01 21:47:24 +0300

    Moving js and css to separate files

---
+++ WebClient/public/app.js
@@ -0,0 +1,21 @@
+$(function () {
+    $('#startTime').datetimepicker({
+    	locale: 'ru'
+    });
+    $('#endTime').datetimepicker({
+    	locale: 'ru',
+        useCurrent: false
+    });
+    $("#startTime").on("dp.change", function (e) {
+        $('#endTime').data("DateTimePicker").minDate(e.date);
+    });
+    $("#endTime").on("dp.change", function (e) {
+        $('#startTime').data("DateTimePicker").maxDate(e.date);
+    });
+	$( "#startTime" ).bind( "click", function() {
+			$("#optionsPeriod2").click()
+	});
+	$( "#endTime" ).bind( "click", function() {
+			$("#optionsPeriod2").click()
+	});
+});
