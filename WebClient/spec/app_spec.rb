bb5d47b7132cb84a808af52381ee9078bef459d2
Oleksii Kozhyn <suburbakskin@gmail.com>
2017-11-09 07:22:27 +0200

    Created correct factory

--- WebClient/spec/app_spec.rb
+++ WebClient/spec/app_spec.rb
@@ -1,19 +1,28 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
 FactoryBot.define do
+  sequence :time do |n|
+    Time.now-(n*3600)
+  end
+
   factory :telemetry, class: Pow do
-    alarm_power "10"
+    factor { (100.0/(120+rand(20))).round(2) }
+    datetime { generate(:time) }
+    voltage { 210+rand(30) }
+    current { (10.0/(15+rand(15))).round(3) }
+    power { voltage*current*factor }
+    alarm_power '100'
+    alarm_on true
   end
 end
 
-describe "WebClient" do
+describe 'WebClient' do
   before(:all) do
-    create(:telemetry)
+    create_list(:telemetry, 72)
   end
 
-  it "should allow accessing the home page" do
+  it 'should allow accessing the home page' do
     get '/'
     expect(last_response).to be_ok
   end
-
 end
