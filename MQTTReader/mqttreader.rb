8c061824354078f0e9b4f29ea53e2318a4eb6847
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-03-18 20:31:59 +0200

    Adding eletro meter id from topic to DB

--- MQTTReader/mqttreader.rb
+++ MQTTReader/mqttreader.rb
@@ -4,6 +4,11 @@ require 'mysql2'
 require './helpers/sender'
 require './helpers/colorizer'
 
+# получение номера счетчика из топика
+def get_elmeter_id(topic)
+  %r{/(.+)/}.match(topic)[1]
+end
+
 # чтение параметров подкючения БД
 client = Mysql2::Client.new(host: ENV['POW_DATA_HOST'],
                             username: ENV['POW_DATA_USER'],
@@ -11,7 +16,7 @@ client = Mysql2::Client.new(host: ENV['POW_DATA_HOST'],
                             port: ENV['POW_DATA_PORT'])
 
 # создание канала отправки сообщения
-sender=Sender.new(send_to: ENV['POW_SMS_TEL'])
+sender = Sender.new(send_to: ENV['POW_SMS_TEL'])
 
 # устанавливаем соединение с MQTT брокером
 MQTT::Client.connect(ENV['POW_MQTT_HOST'], ENV['POW_MQTT_PORT'].to_i) do |c|
@@ -28,12 +33,13 @@ MQTT::Client.connect(ENV['POW_MQTT_HOST'], ENV['POW_MQTT_PORT'].to_i) do |c|
                            ORDER BY id DESC LIMIT 1')
 
     client.query("INSERT INTO sonoff.pow
-                (datetime, power, factor, voltage, current, period, alarm_power, alarm_on)
+                (datetime, power, factor, voltage, current, period, alarm_power, alarm_on, elmeter_id)
                 VALUES
                 ('#{parsed_hash['Time']}', #{parsed_hash['Power']},
                 #{parsed_hash['Factor']}, #{parsed_hash['Voltage']},
                 #{parsed_hash['Current']}, #{parsed_hash['Period']},
-                #{result.first['alarm_power']}, #{result.first['alarm_on']})")
+                #{result.first['alarm_power']}, #{result.first['alarm_on']},
+                '#{get_elmeter_id(topic)}')")
 
     # отправка сообщения пользователю о превышении граничной мощности
     if (result.first['alarm_on'] != 0) && (parsed_hash['Period'] > result.first['alarm_power'])
